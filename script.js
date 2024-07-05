5cc896e90f7d73887c6ecfae198609343647f73f
Joshua Cagasan <jcagasan.personal@gmail.com>
2024-07-06 01:11:36 +0800

    Added tasks and some minor updates.
    Need to finalize this repo and static site ASAP so I can focus on my Fullstack Site soon.

--- script.js
+++ script.js
@@ -1,15 +1,7 @@
 function toggleMenu() {
-    var x = document.getElementById("myLinks");
-    if (x.style.display === "block") {
-        x.style.display = "none";
-    } else {
-        x.style.display = "block";
-    }
-    
-}
-
-var showFormButton = document.getElementById("showForm");
-var formContainer = document.querySelector('.form-container');
-showFormButton.addEventListener('click', () => {
-    formContainer.classList.toggle('active');
-});
+var x = document.getElementById("myLinks");
+if (x.style.display === "block") {
+    x.style.display = "none";
+} else {
+    x.style.display = "block";
+}};
